70112e51a8b9229a05fb405fce022b3cac5f96d8
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2024-04-03 06:18:49 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,7 +7,8 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:${OPENALPR_LIB_DIR}
 
 g++ -Wall -L${OPENALPR_LIB_DIR} -I${OPENALPR_INCLUDE_DIR} -shared -fPIC -o libopenalprgo.so openalprgo.cpp -lopenalpr
 
-#(cd viam-openalpr && go install)
+#(cd openalpr && go install)
+#go run main.go
+
 cd viam-openalpr
 go build -o viamalpr
-#go run module.go
